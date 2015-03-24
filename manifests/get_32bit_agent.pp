6f220fedd56bea7017d020cbdb8919db15261d8a
Josh Samuelson <js@puppetlabs.com>
2015-03-24 15:47:31 -0700

    Cache 32bit agent for offline

--- manifests/get_32bit_agent.pp
+++ manifests/get_32bit_agent.pp
@@ -1,27 +1,37 @@
 # -------
-# Fetch PE and unzip full installer
-# Stage agent installer.
+# Fetch and unzip 32bit agent installer
 # -------
 
 class bootstrap::get_32bit_agent(
   $version   = 'latest',
-  $pe_destination = '/root',
+  $repo_dir = '/opt/puppet/packages/',
   $architecture   = $::architecture,
   $file_cache     = '/vagrant/file_cache'
 ) {
-  $pe_dir        = "puppet-enterprise-${version}-el-${operatingsystemmajrelease}-i386"
-  $agent_file     = "${pe_dir}-agent.tar.gz"
+  $agent_dir        = "puppet-enterprise-${version}-el-${operatingsystemmajrelease}-i386-agent"
+  $agent_file     = "${agent_dir}.tar.gz"
+  $public_dir     = "${repo_dir}/public"
   $url            = "https://s3.amazonaws.com/pe-builds/released/${version}"
 
-  # Check if there is a locally cached copy from the build
-  if file_exists ("${file_cache}/installers/${agent_file}") == 1 {
-    staging::file{ $agent_file:
-      source => "${file_cache}/installers/${agent_file}",
-    }
+  file { $repo_dir:
+    ensure => directory,
   }
-  else {
-    staging::file{ $agent_file:
-      source => "${url}/${agent_file}",
-    }
+  file { "${public_dir}/":
+    ensure => directory,
+  }
+  file { "${public_dir}/${version}":
+    ensure => directory,
+  }
+  staging::deploy { $agent_file:
+    source  => "${url}/${agent_file}",
+    target  => "${public_dir}/",
+    creates => "${public_dir}/${agent_dir}",
+    require => File["${public_dir}/"]
+  }
+  #our nice symlink to make the .repo files happy
+  file { "${public_dir}/${version}/el-${operatingsystemmajrelease}-i386":
+    ensure  => link,
+    target  => "${public_dir}/${agent_dir}/agent_packages/${installer_build}",
+    require => [Staging::Deploy[$agent_file],File["${public_dir}/${version}"]],
   }
 }
