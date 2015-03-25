f0a0db4e9b58bf492d2f7a1108d93e5b4bc48717
Josh Samuelson <js@puppetlabs.com>
2015-03-25 09:52:27 -0700

    Adding puppetdir and consolidate manifest

--- manifests/get_32bit_agent.pp
+++ manifests/get_32bit_agent.pp
@@ -4,34 +4,30 @@
 
 class bootstrap::get_32bit_agent(
   $version   = 'latest',
-  $repo_dir = '/opt/puppet/packages/',
+  $puppet_dir = '/opt/puppet',
   $architecture   = $::architecture,
   $file_cache     = '/vagrant/file_cache'
 ) {
+  $repo_dir       = "${puppet_dir}/packages"
+  $public_dir     = "${repo_dir}/public"
+  $version_dir     = "${public_dir}/${verision}"
   $agent_dir        = "puppet-enterprise-${version}-el-${operatingsystemmajrelease}-i386-agent"
   $agent_file     = "${agent_dir}.tar.gz"
-  $public_dir     = "${repo_dir}/public"
   $url            = "https://s3.amazonaws.com/pe-builds/released/${version}"
 
-  file { $repo_dir:
-    ensure => directory,
-  }
-  file { "${public_dir}/":
-    ensure => directory,
-  }
-  file { "${public_dir}/${version}":
-    ensure => directory,
+  file { [$puppet_dir,$repo_dir,$public_dir,$version_dir]:
+    ensure => directory
   }
   staging::deploy { $agent_file:
     source  => "${url}/${agent_file}",
-    target  => "${public_dir}/",
+    target  => $public_dir,
     creates => "${public_dir}/${agent_dir}",
-    require => File["${public_dir}/"]
+    require => File[$public_dir]
   }
   #our nice symlink to make the .repo files happy
-  file { "${public_dir}/${version}/el-${operatingsystemmajrelease}-i386":
+  file { "${version_dir}/el-${operatingsystemmajrelease}-i386":
     ensure  => link,
     target  => "${public_dir}/${agent_dir}/agent_packages/${installer_build}",
-    require => [Staging::Deploy[$agent_file],File["${public_dir}/${version}"]],
+    require => [Staging::Deploy[$agent_file],File[$version_dir]],
   }
 }
