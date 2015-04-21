4a6965e9406c71e4c8d46d59230638c56ba5b983
Josh Samuelson <js@puppetlabs.com>
2015-04-21 15:27:05 -0700

    Get agent manifest from LMS

---
+++ manifests/get_agent.pp
@@ -0,0 +1,34 @@
+# -------
+# Fetch and unzip agent installer
+# -------
+
+class bootstrap::get_agent(
+  $version        = '3.7.2',
+  $architecture   = $::architecture,
+  $file_cache     = '/vagrant/file_cache'
+) {
+  $puppet_dir   = '/opt/puppet'
+  $repo_dir     = "${puppet_dir}/packages"
+  $public_dir   = "${repo_dir}/public"
+  $version_dir  = "${public_dir}/${version}"
+  $agent_dir    = "puppet-enterprise-${version}-el-${operatingsystemmajrelease}-${architecture}-agent"
+  $agent_file   = "${agent_dir}.tar.gz"
+  $url          = "https://s3.amazonaws.com/pe-builds/released/${version}"
+  $installer_build = "el-${operatingsystemmajrelease}-${architecture}"
+
+  file { [$puppet_dir,$repo_dir,$public_dir,$version_dir]:
+    ensure => directory
+  }
+  staging::deploy { $agent_file:
+    source  => "${url}/${agent_file}",
+    target  => $public_dir,
+    creates => "${public_dir}/${agent_dir}",
+    require => File[$public_dir]
+  }
+  #our nice symlink to make the .repo files happy
+  file { "${version_dir}/${installer_build}":
+    ensure  => link,
+    target  => "${public_dir}/${agent_dir}/agent_packages/${installer_build}",
+    require => [Staging::Deploy[$agent_file],File[$version_dir]],
+  }
+}
