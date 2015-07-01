6d102c1a4897b191da98701ce6ffe92d2f223607
Josh Samuelson <js@puppetlabs.com>
2015-07-01 16:14:07 -0700

    Moving network stuff to new class

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,26 +21,10 @@ class bootstrap {
     mode   => 0755,
   }
 
-  # Make sure the firewall isn't running
-  service { 'iptables':
-    enable => false,
-    ensure => stopped,
-  }
   # Add a few extra packages for convenience
   package { [ 'patch', 'screen', 'telnet', 'tree' ] :
     ensure  => present,
     require => Class['localrepo'],
-  }
-
-  file { '/etc/sysconfig/network':
-    ensure  => file,
-    content => template('bootstrap/network.erb'),
-  }
-  service { 'network':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/sysconfig/network'],
-    hasstatus => true,
   }
 
   # /etc/puppet/ssl is confusing to have around. Sloppy. Kill.
@@ -48,13 +32,6 @@ class bootstrap {
     ensure  => absent,
     recurse => true,
     force   => true,
-  }
-
-  # Remove the udev ethernet naming rules, causes problems when
-  # moving VMs around. This works for rhel/centos
-  file {'/etc/udev/rules.d/70-persistent-net.rules':
-    ensure   => absent,
-    force    => true,
   }
 
   include bootstrap::ruby
