e87761ee86a0b0472c6bfeb72be85d3bff9a2311
Josh Samuelson <js@puppetlabs.com>
2015-07-02 15:16:58 -0700

    typo

--- manifests/profile/learning_splash.pp
+++ manifests/profile/learning_splash.pp
@@ -1,5 +1,5 @@
 class bootstrap::profile::learning_splash {
-  class {'bootstrap::profiles::splash':
+  class {'bootstrap::profile::splash':
     login_message => 'To view the quest guide, go to http://<YOUR IP>'
   }
 }
