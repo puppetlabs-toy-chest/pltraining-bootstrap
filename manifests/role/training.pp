316ff04b27c051a1d8f25bddff869fc5ebd6f280
Josh Samuelson <js@puppetlabs.com>
2015-07-02 13:45:36 -0700

    Cache wordpress on training

--- manifests/role/training.pp
+++ manifests/role/training.pp
@@ -1,5 +1,6 @@
 class bootstrap::role::training {
   include bootstrap
+  include bootstrap::profile::cache_wordpress
   include bootstrap::profile::scripts
   include bootstrap::profile::classroom_ssh
   include bootstrap::profile::get_32bit_agent
