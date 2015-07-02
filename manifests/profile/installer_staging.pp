1f3aa0c409b3105cbd41d9a8feb00251f510d19e
Josh Samuelson <js@puppetlabs.com>
2015-07-02 15:05:23 -0700

    Set staging up before installing pe

--- manifests/profile/installer_staging.pp
+++ manifests/profile/installer_staging.pp
@@ -1,7 +1,8 @@
 class bootstrap::profile::installer_staging {
   class { 'staging':
-    path  => '/usr/src/installer/',
-    owner => 'root',
-    group => 'root',
+    path   => '/usr/src/installer/',
+    owner  => 'root',
+    group  => 'root',
+    before => Class['install_pe'],
   }
 }
