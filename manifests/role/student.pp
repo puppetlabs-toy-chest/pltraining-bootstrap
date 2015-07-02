c801495c5634bf190880004a7fbf8e776fbc6dc4
Josh Samuelson <js@puppetlabs.com>
2015-07-02 12:16:47 -0700

    Using the same yum config for student VM

--- manifests/role/student.pp
+++ manifests/role/student.pp
@@ -1,4 +1,5 @@
 class bootstrap::role::student {
+  include bootstrap::profile::yum
   include bootstrap::profile::base_ssh
   include bootstrap::profile::network
   include bootstrap::profile::cache_modules
