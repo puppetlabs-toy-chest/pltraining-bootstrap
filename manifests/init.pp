fc872f15898db110934a598efb68c8b11f39f1f9
Josh Samuelson <js@puppetlabs.com>
2015-07-02 10:22:12 -0700

    Moving base items to new class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,55 +1,11 @@
 class bootstrap {
-  File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-  }
-
-  # Moving the root user declaration to the userprefs module.
-  # user { 'root':
-  #   password => '$1$hgIZHl1r$tEqMTzoXz.NBwtW3kFv33/',
-  # }
-  file { '/usr/bin/envpuppet':
-    source => 'puppet:///modules/bootstrap/envpuppet',
-    mode   => '0755',
-  }
-
-  # This shouldn't change anything, but want to make sure it actually IS laid out the way I expect.
-  file {'/etc/rc.local':
-    ensure => symlink,
-    target => 'rc.d/rc.local',
-    mode   => 0755,
-  }
-
-  # Add a few extra packages for convenience
-  package { [ 'patch', 'screen', 'telnet', 'tree' ] :
-    ensure  => present,
-    require => Class['localrepo'],
-  }
-
-  # /etc/puppet/ssl is confusing to have around. Sloppy. Kill.
-  file {'/etc/puppet':
-    ensure  => absent,
-    recurse => true,
-    force   => true,
-  }
-
+  include bootstrap::base
   include bootstrap::ruby
-
-  # Cache forge modules locally in the vm:
-  class { 'bootstrap::cache_modules': cache_dir => '/usr/src/forge' }
-
-  # Cache gems locally in the vm:
-  class { 'bootstrap::cache_gems': }
-
-  # configure user environment
+  include bootstrap::cache_modules
+  include bootstrap::cache_gems
   include userprefs::defaults
-
   include bootstrap::splash
-
   include bootstrap::yum
-
   include bootstrap::ssh
-
   include bootstrap::network
 }
