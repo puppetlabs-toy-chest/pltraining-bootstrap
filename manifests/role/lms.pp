2825209516debafdb3cf803b0431dd91b2119202
Josh Samuelson <js@puppetlabs.com>
2015-07-07 14:55:50 -0700

    LMS hiera defaults

---
+++ manifests/role/lms.pp
@@ -0,0 +1,19 @@
+class bootstrap::role:lms {
+  include epel
+  include localrepo
+  include lms::scripts
+  include lms::lab_deps
+  include lms::install_pe
+  include userprefs::defaults
+  include bootstrap::profile::yum
+  include bootstrap::profile::base
+  include bootstrap::profile::ruby
+  include bootstrap::profile::get_pe
+  include bootstrap::profile::network
+  include bootstrap::profile::lms_base
+  include bootstrap::profile::cache_gems
+  include bootstrap::profile::lms_defaults
+  include bootstrap::profile::classroom_ssh
+  include bootstrap::profile::cache_modules
+  include bootstrap::profile::installer_staging
+}
