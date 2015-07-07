4e1a2f6d362737855477bdee34368cc7e74f9d86
Josh Samuelson <js@puppetlabs.com>
2015-07-07 10:39:10 -0700

    Pixel pushing splash screen

--- manifests/profile/learning_splash.pp
+++ manifests/profile/learning_splash.pp
@@ -1,6 +1,6 @@
 class bootstrap::profile::learning_splash {
   class {'bootstrap::profile::splash':
     # Note: the $IP_ADDRESS string is a variable determined at boot time by rc.local
-    login_message => 'Quest Guide: http://$IP_ADDRESS'
+    login_message => file('bootstrap/learning_message')
   }
 }
