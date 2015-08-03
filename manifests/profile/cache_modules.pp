3520086fbbad61e330a87ddb8f4f7aabba9e77a3
Josh Samuelson <js@puppetlabs.com>
2015-08-03 11:31:55 -0700

    Updating modules to latest versions

--- manifests/profile/cache_modules.pp
+++ manifests/profile/cache_modules.pp
@@ -16,39 +16,39 @@ class bootstrap::profile::cache_modules(
   #       we use get updated to use this.
   bootstrap::forge { 'badgerious-windows_env':          version => '2.2.1'}
   bootstrap::forge { 'chocolatey-chocolatey':           version => '0.5.3'}
-  bootstrap::forge { 'domcleal-augeasproviders':        version => '1.2.0'}
+  bootstrap::forge { 'domcleal-augeasproviders':        version => '1.2.1'}
   bootstrap::forge { 'dwerder-graphite': }
-  bootstrap::forge { 'garethr-docker':                  version => '4.0.2'}
+  bootstrap::forge { 'garethr-docker':                  version => '4.1.0'}
   bootstrap::forge { 'hunner-charybdis':                version => '1.0.0'}
   bootstrap::forge { 'hunner-wordpress':                version => '0.6.0'}
   bootstrap::forge { 'jamtur01-irc':                    version => '0.0.7'}
   bootstrap::forge { 'jordan-fileshare':                version => '1.1.1'}
   bootstrap::forge { 'jriviere-windows_ad':             version => '0.2.0'}
-  bootstrap::forge { 'nanliu-staging':                  version => '1.0.2'}
+  bootstrap::forge { 'nanliu-staging':                  version => '1.0.3'}
   bootstrap::forge { 'opentable-download_file':         version => '1.1.0'}
   bootstrap::forge { 'opentable-windowsfeature':        version => '1.0.0'}
-  bootstrap::forge { 'pltraining-classroom':            version => '1.2.6'}
+  bootstrap::forge { 'pltraining-classroom':            version => '1.3.2'}
   bootstrap::forge { 'pltraining-dirtree':              version => '0.2.2'}
-  bootstrap::forge { 'pltraining-rbac':                 version => '0.0.3'}
+  bootstrap::forge { 'pltraining-rbac':                 version => '0.0.4'}
   bootstrap::forge { 'pltraining-userprefs':}
-  bootstrap::forge { 'puppetlabs-acl':                  version => '1.1.0'}
-  bootstrap::forge { 'puppetlabs-apache':               version => '1.2.0'}
+  bootstrap::forge { 'puppetlabs-acl':                  version => '1.1.1'}
+  bootstrap::forge { 'puppetlabs-apache':               version => '1.6.0'}
   bootstrap::forge { 'puppetlabs-concat':               version => '1.2.4'}
-  bootstrap::forge { 'puppetlabs-git':                  version => '0.2.0'}
-  bootstrap::forge { 'puppetlabs-haproxy':              version => '1.0.0'}
-  bootstrap::forge { 'puppetlabs-inifile':              version => '1.2.0'}
-  bootstrap::forge { 'puppetlabs-mysql':                version => '2.3.1'}
-  bootstrap::forge { 'puppetlabs-ntp':                  version => '3.2.1'}
-  bootstrap::forge { 'puppetlabs-pe_gem':               version => '0.0.1'}
+  bootstrap::forge { 'puppetlabs-git':                  version => '0.4.0'}
+  bootstrap::forge { 'puppetlabs-haproxy':              version => '1.3.0'}
+  bootstrap::forge { 'puppetlabs-inifile':              version => '1.4.1'}
+  bootstrap::forge { 'puppetlabs-mysql':                version => '3.5.0'}
+  bootstrap::forge { 'puppetlabs-ntp':                  version => '4.1.0'}
+  bootstrap::forge { 'puppetlabs-pe_gem':               version => '0.1.1'}
   bootstrap::forge { 'puppetlabs-pe_puppetserver_gem':  version => '0.0.1'}
   bootstrap::forge { 'puppetlabs/puppetserver_gem':     version => '0.1.0'}
   bootstrap::forge { 'puppetlabs-reboot':               version => '0.1.9'}
   bootstrap::forge { 'puppetlabs-registry':             version => '1.1.0'}
   bootstrap::forge { 'puppetlabs-stdlib':               version => '4.7.0'}
-  bootstrap::forge { 'puppetlabs-vcsrepo':              version => '1.1.0'}
-  bootstrap::forge { 'razorsedge-vmwaretools':          version => '4.7.0'}
+  bootstrap::forge { 'puppetlabs-vcsrepo':              version => '1.3.0'}
+  bootstrap::forge { 'razorsedge-vmwaretools':          version => '5.0.1'}
   bootstrap::forge { 'stahnma-epel':                    version => '1.0.2'}
   bootstrap::forge { 'thias-vsftpd':                    version => '0.2.1'}
   bootstrap::forge { 'zack-exports':                    version => '0.0.4'}
-  bootstrap::forge { 'zack-r10k':                       version => '2.3.0'}
+  bootstrap::forge { 'zack-r10k':                       version => '3.0.0'}
 }
