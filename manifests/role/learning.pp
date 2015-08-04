0a0fc7dae9bfd780a6fe0f6029d20b28e8db96ae
Josh Samuelson <js@puppetlabs.com>
2015-08-04 16:40:40 -0700

    Dep for course_selector

--- manifests/role/learning.pp
+++ manifests/role/learning.pp
@@ -3,6 +3,7 @@ class bootstrap::role::learning {
   include localrepo
   include bootstrap
   include lms::course_selector
+  include lms::lab_repo
   include bootstrap::profile::quest_content
   include bootstrap::profile::installer_staging
   include bootstrap::profile::learning_ssh
