b4b35f9fbdd422084ff1364cd03380558b1e354f
Josh Samuelson <js@puppetlabs.com>
2015-08-04 16:25:02 -0700

    Install course_selector on learning vm

--- manifests/role/learning.pp
+++ manifests/role/learning.pp
@@ -2,6 +2,7 @@ class bootstrap::role::learning {
   include epel
   include localrepo
   include bootstrap
+  include lms::course_selector
   include bootstrap::profile::quest_content
   include bootstrap::profile::installer_staging
   include bootstrap::profile::learning_ssh
