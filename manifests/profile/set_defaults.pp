e57e3886afdf7741e8f81fdce0e3c87577c378f2
Josh Samuelson <js@puppetlabs.com>
2015-08-05 13:39:01 -0700

    Correct location for hieradata

--- manifests/profile/set_defaults.pp
+++ manifests/profile/set_defaults.pp
@@ -1,14 +1,14 @@
 # Add default memory settings after PE install
 
 class bootstrap::profile::set_defaults {
-  file { '/etc/puppetlabs/puppet/hieradata':
+  file { '/etc/puppetlabs/code/hieradata':
     ensure => directory,
     require => Class['bootstrap::profile::install_pe'],
   }
-  file { '/etc/puppetlabs/puppet/hieradata/defaults.yaml':
+  file { '/etc/puppetlabs/code/hieradata/defaults.yaml':
     ensure => present,
     source => 'puppet:///modules/bootstrap/defaults.yaml',
-    require => File['/etc/puppetlabs/puppet/hieradata'],
+    require => File['/etc/puppetlabs/code/hieradata'],
   }
 }
 
