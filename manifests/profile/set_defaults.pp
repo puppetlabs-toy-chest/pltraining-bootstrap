4f6bc05a1604ea43a8292f73dd5a2ae85ad34ab7
Josh Samuelson <js@puppetlabs.com>
2015-08-05 14:13:50 -0700

    Put hiera files in place before pe install

--- manifests/profile/set_defaults.pp
+++ manifests/profile/set_defaults.pp
@@ -1,13 +1,16 @@
 # Add default memory settings after PE install
 
 class bootstrap::profile::set_defaults {
+  exec { 'mkdir -p /etc/puppetlabs/code':
+    path   => '/bin',
+    before => [File['/etc/puppetlabs/code/hiera.yaml','/etc/puppetlabs/code/hieradata'],Class['bootstrap::install_pe']]
+  }
   file { '/etc/puppetlabs/code/hiera.yaml':
-    ensure => file,
-    source => 'puppet:///modules/bootstrap/hiera.yaml',
+    ensure  => file,
+    source  => 'puppet:///modules/bootstrap/hiera.yaml',
   }
   file { '/etc/puppetlabs/code/hieradata':
     ensure => directory,
-    require => Class['bootstrap::profile::install_pe'],
   }
   file { '/etc/puppetlabs/code/hieradata/defaults.yaml':
     ensure => file,
