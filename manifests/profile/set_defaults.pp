43eb6ddbeb3b4665f226d8180254081df2d78085
Josh Samuelson <js@puppetlabs.com>
2015-08-05 13:45:50 -0700

    default hiera

--- manifests/profile/set_defaults.pp
+++ manifests/profile/set_defaults.pp
@@ -1,12 +1,16 @@
 # Add default memory settings after PE install
 
 class bootstrap::profile::set_defaults {
+  file { '/etc/puppetlabs/code/hiera.yaml':
+    ensure => file,
+    source => 'puppet:///modules/bootstrap/hiera.yaml',
+  }
   file { '/etc/puppetlabs/code/hieradata':
     ensure => directory,
     require => Class['bootstrap::profile::install_pe'],
   }
   file { '/etc/puppetlabs/code/hieradata/defaults.yaml':
-    ensure => present,
+    ensure => file,
     source => 'puppet:///modules/bootstrap/defaults.yaml',
     require => File['/etc/puppetlabs/code/hieradata'],
   }
