c12ad40082ceb980c0738535b67faa41e59723bd
Josh Samuelson <js@puppetlabs.com>
2015-08-12 14:33:56 -0700

    Replacement for factor command

---
+++ files/factor.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "-------------------------------------------------------------------------"
+echo "                You typed 'factor' instead of 'facter'."
+echo "          'factor' calculates the prime factors of a number."
+echo ""
+echo "      If you need to run the math related command use: /usr/bin/factor"
+echo "               To display facts about your system use: facter"
+echo "-------------------------------------------------------------------------"
