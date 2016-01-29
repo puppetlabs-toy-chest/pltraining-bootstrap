26a3ea1878473d5c7e222cadb0cd476ba26eda97
Josh Samuelson <js@puppetlabs.com>
2016-01-29 15:36:13 -0800

    Get file from the right place

--- manifests/public_key.pp
+++ manifests/public_key.pp
@@ -2,6 +2,6 @@ class bootstrap::public_key ( $admin_user = $bootstrap::params::admin_user ) {
   ssh_authorized_key { 'instructor':
     user => $admin_user,
     type => 'ssh_rsa',
-    key  => file('puppet:///bootstrap/training.pub'),
+    key  => file('puppet:///bootstrap/files/training.pub'),
   }
 }
