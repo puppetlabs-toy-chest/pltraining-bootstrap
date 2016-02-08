d71b20634d2d039c644e9044064509d997222087
Josh Samuelson <js@puppetlabs.com>
2016-02-08 15:50:24 -0800

    Install agent files for el7

--- manifests/role/puppetfactory.pp
+++ manifests/role/puppetfactory.pp
@@ -3,6 +3,7 @@ class bootstrap::role::puppetfactory {
   include localrepo
   include bootstrap
   include pe_repo::platform::el_6_i386
+  include pe_repo::platform::el_7_x86_64
   include bootstrap::profile::splash
   include bootstrap::public_key
   include bootstrap::profile::scripts
