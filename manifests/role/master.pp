82cdd7ae840d091c100f87fb3868b0ecc23e47a2
Josh Samuelson <js@puppetlabs.com>
2016-02-08 15:55:56 -0800

    Add agent repo for ubuntu 14

--- manifests/role/master.pp
+++ manifests/role/master.pp
@@ -4,6 +4,7 @@ class bootstrap::role::master {
   include bootstrap
   include pe_repo::platform::el_6_i386
   include pe_repo::platform::el_7_x86_64
+  include pe_repo::platform::ubuntu_1404_x86_64
   include bootstrap::profile::splash
   include bootstrap::public_key
   include bootstrap::profile::scripts
