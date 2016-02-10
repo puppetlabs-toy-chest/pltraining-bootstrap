6825360a5e20eb8d84c03e9713aafe1b5897415d
Josh Samuelson <js@puppetlabs.com>
2016-02-10 11:41:07 -0800

    Use parameter instead of profile

--- manifests/role/learning.pp
+++ manifests/role/learning.pp
@@ -2,8 +2,11 @@ class bootstrap::role::learning {
   include epel
   include localrepo
   include bootstrap
-  include bootstrap::profile::learning_splash
   class {'learning':
     git_branch => 'master',
+  }
+  class {'bootstrap::profile::splash':
+    # Note: the $IP_ADDRESS string is a variable determined at boot time by rc.local
+    login_message => file('bootstrap/learning_message')
   }
 }
