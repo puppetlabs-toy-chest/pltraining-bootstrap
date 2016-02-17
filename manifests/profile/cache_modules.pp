205dd39bde975cc00f31411e112985a3e77723da
Josh Samuelson <js@puppetlabs.com>
2016-02-17 10:52:58 -0800

    Use puppet module tool to cache latest modules

--- manifests/profile/cache_modules.pp
+++ manifests/profile/cache_modules.pp
@@ -9,47 +9,10 @@ class bootstrap::profile::cache_modules(
     ensure => directory,
   }
 
-  # These are the modules needed by the Fundamentals course
-
-  # This is a temporary kludge as we transition from ripienaar-concat to puppetlabs
-  # TODO: revisit this regularly and dump it when PE ships with this and all modules
-  #       we use get updated to use this.
-  bootstrap::forge { 'badgerious-windows_env':          version => '2.2.1'}
-  bootstrap::forge { 'chocolatey-chocolatey':           version => '0.5.3'}
-  bootstrap::forge { 'domcleal-augeasproviders':        version => '1.2.1'}
-  bootstrap::forge { 'dwerder-graphite': }
-  bootstrap::forge { 'garethr-docker':                  version => '4.1.0'}
-  bootstrap::forge { 'hunner-charybdis':                version => '1.0.0'}
-  bootstrap::forge { 'hunner-wordpress':                version => '0.6.0'}
-  bootstrap::forge { 'jamtur01-irc':                    version => '0.0.7'}
-  bootstrap::forge { 'jordan-fileshare':                version => '1.1.1'}
-  bootstrap::forge { 'jriviere-windows_ad':             version => '0.2.0'}
-  bootstrap::forge { 'nanliu-staging':                  version => '1.0.3'}
-  bootstrap::forge { 'puppet-download_file':            version => '1.2.1'}
-  bootstrap::forge { 'puppet-windowsfeature':           version => '1.1.0'}
-  bootstrap::forge { 'pltraining-classroom':            version => '1.3.3'}
-  bootstrap::forge { 'pltraining-dirtree':              version => '0.2.2'}
-  bootstrap::forge { 'pltraining-rbac':                 version => '0.0.4'}
-  bootstrap::forge { 'pltraining-userprefs':}
-  bootstrap::forge { 'puppetlabs-acl':                  version => '1.1.1'}
-  bootstrap::forge { 'puppetlabs-apache':               version => '1.6.0'}
-  bootstrap::forge { 'puppetlabs-concat':               version => '1.2.4'}
-  bootstrap::forge { 'puppetlabs-git':                  version => '0.4.0'}
-  bootstrap::forge { 'puppetlabs-haproxy':              version => '1.3.0'}
-  bootstrap::forge { 'puppetlabs-inifile':              version => '1.4.1'}
-  bootstrap::forge { 'puppetlabs-mysql':                version => '3.5.0'}
-  bootstrap::forge { 'puppetlabs-ntp':                  version => '4.1.0'}
-  bootstrap::forge { 'puppetlabs-pe_gem':               version => '0.1.1'}
-  bootstrap::forge { 'puppetlabs-pe_puppetserver_gem':  version => '0.0.1'}
-  bootstrap::forge { 'puppetlabs/puppetserver_gem':     version => '0.1.0'}
-  bootstrap::forge { 'puppetlabs-reboot':               version => '0.1.9'}
-  bootstrap::forge { 'puppetlabs-registry':             version => '1.1.0'}
-  bootstrap::forge { 'puppetlabs-stdlib':               version => '4.7.0'}
-  bootstrap::forge { 'puppetlabs-vcsrepo':              version => '1.3.0'}
-  bootstrap::forge { 'razorsedge-vmwaretools':          version => '5.0.1'}
-  bootstrap::forge { 'stahnma-epel':                    version => '1.0.2'}
-  bootstrap::forge { 'thias-vsftpd':                    version => '0.2.1'}
-  bootstrap::forge { 'zack-exports':                    version => '0.0.4'}
-  bootstrap::forge { 'zack-r10k':                       version => '3.0.0'}
-  bootstrap::forge { 'puppetlabs-aws':                  version => '1.1.1'}
+  exec { 'cache modules':
+    command => "puppet module install pltraining-classroom --modulepath=${cachedir}",
+    creates => "${cachedir}/classroom",
+    path    => '/opt/puppetlabs/bin',
+    require => File[$cache_dir],
+  }
 }
