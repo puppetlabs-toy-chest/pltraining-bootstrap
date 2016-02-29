8cce39f198f8a79e6bb8381c91e1d11e566cac5d
Josh Samuelson <js@puppetlabs.com>
2016-02-29 12:37:10 -0800

    Add parameter to allow pre-release testing

--- manifests/role/training.pp
+++ manifests/role/training.pp
@@ -1,9 +1,13 @@
-class bootstrap::role::training {
+class bootstrap::role::training (
+  $pe_version = $bootstrap::params::pe_version
+) {
   include epel
   include localrepo
   include bootstrap
   include bootstrap::profile::splash
   include bootstrap::profile::cache_wordpress
   include bootstrap::profile::installer_staging
-  include bootstrap::profile::get_pe
+  class { 'bootstrap::profile::get_pe':
+    pe_version => $pe_version
+  }
 }
