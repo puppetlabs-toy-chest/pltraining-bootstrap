5a240c0108e7fb8e7c8137ec20e636295ee2ef49
Josh Samuelson <js@puppetlabs.com>
2016-03-14 14:41:36 -0700

    missing comma

--- manifests/profile/pe_tweaks.pp
+++ manifests/profile/pe_tweaks.pp
@@ -18,7 +18,7 @@ class bootstrap::profile::pe_tweaks {
   }
 
   package { ['trollop','serverspec','rspec-its','rspec-core','rspec']:
-    ensure   => present
+    ensure   => present,
     provider => 'pe_gem',
   }
 
