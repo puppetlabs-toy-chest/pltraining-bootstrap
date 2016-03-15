ca2a37a2eadc3f0635c1a16be10dc8edec452810
Josh Samuelson <js@puppetlabs.com>
2016-03-15 12:44:56 -0700

    Add password reset script

---
+++ files/scripts/classroom/reset_password.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/ruby
+# Reset root password and update login screen
+
+print "Enter new root password: "
+password = gets.chomp
+
+%x(echo "root:#{password}"|chpasswd)
+
+File.open('/var/local/password','w') do |f|
+  f.puts password
+end
+
+%(/etc/rc.local 2>/dev/null)
