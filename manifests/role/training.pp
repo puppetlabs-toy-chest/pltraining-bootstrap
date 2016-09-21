bbb0a9d80be16455a5acedd4de860b295423e39e
kjhenner <kjhenner@gmail.com>
2016-09-21 10:03:52 -0700

    move userprefs to roles and disable line numbering for learnign

--- manifests/role/training.pp
+++ manifests/role/training.pp
@@ -3,6 +3,7 @@ class bootstrap::role::training (
 ) inherits bootstrap::params {
   include localrepo
   include bootstrap
+  include userprefs::defaults
   include bootstrap::profile::splash
   include bootstrap::profile::cache_wordpress
   include bootstrap::profile::installer_staging
