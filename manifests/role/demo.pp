f78471920cbf2aafd6faebd53aab0206b77b66e8
marrero984 <michael.marrero@puppetlabs.com>
2016-09-28 11:03:04 -0700

    add abalone and fix one typo

--- manifests/role/demo.pp
+++ manifests/role/demo.pp
@@ -4,6 +4,7 @@ class bootstrap::role::learning {
   include bootstrap::profile::network
   include bootstrap::profile::pe_tweaks
   include userprefs::profile
+  include abalone
   class { 'userprefs::bash':
     password => '$1$hgIZHl1r$tEqMTzoXz.NBwtW3kFv33/',
     replace  => true,
@@ -13,8 +14,8 @@ class bootstrap::role::learning {
     require     => Class['userprefs::profile'],
   }
   class {'learning':
-    git_branch => 'master',
-    content_repo_name = 'puppetconf-edu-demo'
+    git_branch        => 'master',
+    content_repo_name => 'puppetconf-edu-demo'
   }
   class {'bootstrap::profile::splash':
     # Note: the $IP_ADDRESS string is a variable determined at boot time by rc.local
