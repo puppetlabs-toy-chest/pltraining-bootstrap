c581c41f6f4d02d3ace691d3df22644575be1211
Josh Samuelson <js@puppet.com>
2016-10-06 12:14:13 -0700

    Gogs port

--- manifests/profile/gogs.pp
+++ manifests/profile/gogs.pp
@@ -3,7 +3,7 @@ class bootstrap::profile::gogs {
   include docker
   docker::run {'ciab-gogs':
     image            => 'gogs/gogs',
-    ports            => ['10022:22','10080:3000'],
+    ports            => ['10022:22','3000:3000'],
     volumes          => ['/var/gogs:/data'],
   }
 }
