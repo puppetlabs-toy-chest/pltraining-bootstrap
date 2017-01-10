0721eb542f04be387a131c1df85c35208db9bc5d
Greg Larkin <greg.larkin@puppet.com>
2017-01-10 10:39:51 -0800

    Remove libvirt configuration for now

--- manifests/role/ciab.pp
+++ manifests/role/ciab.pp
@@ -4,7 +4,7 @@ class bootstrap::role::ciab (
   include localrepo
   include bootstrap
   include userprefs::defaults
-  include bootstrap::profile::virt
+  # include bootstrap::profile::virt
   include bootstrap::profile::splash
   include bootstrap::profile::guacamole
   include bootstrap::profile::nginx_proxy
