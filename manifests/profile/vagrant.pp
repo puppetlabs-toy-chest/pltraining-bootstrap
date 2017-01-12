0a19b0958b35f6ef3914771ad0420954a8d4f7cf
Greg Larkin <greg.larkin@puppet.com>
2017-01-12 09:38:59 -0800

    Added resources to add training vagrant boxes

--- manifests/profile/vagrant.pp
+++ manifests/profile/vagrant.pp
@@ -1,8 +1,24 @@
 class bootstrap::profile::vagrant {
-  include ::virtualbox
+  include virtualbox
 
   class { 'vagrant':
     version => '1.9.1',
-    require => Class['::virtualbox'],
+    require => Class['virtualbox'],
+  }
+
+  # Convert the latest Puppet master instructor OVA and install as a
+  # Vagrant box
+  vagrant::box { 'current-puppet-master-ova':
+    user   => 'training',
+    source => 'http://downloads.puppetlabs.com/training/puppet-master.ova',
+    require => Class['vagrant'],
+  }
+
+  # Convert the latest Puppet master instructor OVA and install as a
+  # Vagrant box
+  vagrant::box { 'current-puppet-student-ova':
+    user   => 'training',
+    source => 'http://downloads.puppetlabs.com/training/puppet-student.ova',
+    require => Class['vagrant'],
   }
 }
