4492a2a4abffe88c9f9457960d2e583c8e13067d
Greg Larkin <greg.larkin@puppet.com>
2017-01-13 12:09:52 -0800

    Added support for specifying a dynamic number of student Vagrant environments in the CIAB solution

--- manifests/profile/vagrant.pp
+++ manifests/profile/vagrant.pp
@@ -1,4 +1,11 @@
 class bootstrap::profile::vagrant {
+  File {
+    ensure => file,
+    owner  => 'training',
+    group  => 'training',
+    mode   => '0644',
+  }
+
   include virtualbox
 
   class { 'vagrant':
@@ -31,4 +38,30 @@ class bootstrap::profile::vagrant {
     source => 'http://downloads.puppetlabs.com/training/puppet-student.ova',
     require => Class['vagrant'],
   }
+
+  # Configure the Oscar environment with the proper files to provision
+  # a master Vagrant box and the required number of student Vagrant boxes
+  $ciab_vagrant_root = '/home/training/classroom_in_a_box'
+
+  file { [ $ciab_vagrant_root, "$ciab_vagrant_root/config" ]:
+    ensure => directory,
+  }
+
+  file { "$ciab_vagrant_root/Vagrantfile":
+    source => 'puppet:///modules/bootstrap/classroom_in_a_box/Vagrantfile',
+  }
+
+  file { "$ciab_vagrant_root/config/roles.yaml":
+    source => 'puppet:///modules/bootstrap/classroom_in_a_box/roles.yaml',
+  }
+
+  file { "$ciab_vagrant_root/config/pe_build.yaml":
+    content => epp('bootstrap/classroom_in_a_box/pe_build.yaml.epp'),
+  }
+
+  file { "$ciab_vagrant_root/config/vms.yaml":
+    content => epp('bootstrap/classroom_in_a_box/vms.yaml.epp',
+                   { num_students => $::num_students }),
+  }
+
 }
