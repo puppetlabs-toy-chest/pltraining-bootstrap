43210437e63835e3413176cca4540cd6687519d0
Ben Ford <ben.ford@puppetlabs.com>
2017-02-01 11:07:17 -0800

    temporarily cache both

--- manifests/profile/cache_docker.pp
+++ manifests/profile/cache_docker.pp
@@ -1,6 +1,7 @@
 class bootstrap::profile::cache_docker {
 
   include docker
+  docker::image { 'maci0/systemd':} # TODO: remove when releasing v5.13
   docker::image { 'centos:7':}
   docker::image { 'phusion/baseimage':}
 
