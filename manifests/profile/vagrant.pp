3128cc2c35ef362ad1c982b5065ba81918d154ac
Greg Larkin <greg.larkin@puppet.com>
2017-02-07 13:24:53 -0800

    Align the hashrockets

--- manifests/profile/vagrant.pp
+++ manifests/profile/vagrant.pp
@@ -23,23 +23,23 @@ class bootstrap::profile::vagrant {
   ]
 
   vagrant::plugin { $vagrant_plugins:
-    user => 'training',
+    user    => 'training',
     require => Class['vagrant'],
   }
 
   # Convert the latest Puppet master instructor OVA and install as a
   # Vagrant box
   vagrant::box { 'current-puppet-master-ova':
-    user   => 'training',
-    source => 'http://downloads.puppetlabs.com/training/puppet-master.ova',
+    user    => 'training',
+    source  => 'http://downloads.puppetlabs.com/training/puppet-master.ova',
     require => Class['vagrant'],
   }
 
   # Convert the latest Puppet master instructor OVA and install as a
   # Vagrant box
   vagrant::box { 'current-puppet-student-ova':
-    user   => 'training',
-    source => 'http://downloads.puppetlabs.com/training/puppet-student.ova',
+    user    => 'training',
+    source  => 'http://downloads.puppetlabs.com/training/puppet-student.ova',
     require => Class['vagrant'],
   }
 
