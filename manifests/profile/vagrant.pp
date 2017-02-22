b1c6e2f78ad0189ac2b9a5357e8297e85602bb5e
Greg Larkin <greg.larkin@puppet.com>
2017-02-22 13:56:00 -0800

    Refactored some exec commands into a script and linted some other bits of code

--- manifests/profile/vagrant.pp
+++ manifests/profile/vagrant.pp
@@ -49,33 +49,43 @@ class bootstrap::profile::vagrant {
   # a master Vagrant box and the required number of student Vagrant boxes
   $ciab_vagrant_root = "${training_home_path}/classroom_in_a_box"
 
-  file { [ $ciab_vagrant_root, "$ciab_vagrant_root/config" ]:
+  file { [ $ciab_vagrant_root, "${ciab_vagrant_root}/config",
+           "${ciab_vagrant_root}/bin" ]:
     ensure => directory,
   }
 
-  file { "$ciab_vagrant_root/Vagrantfile":
+  file { "${ciab_vagrant_root}/Vagrantfile":
     source => 'puppet:///modules/bootstrap/classroom_in_a_box/Vagrantfile',
   }
 
-  file { "$ciab_vagrant_root/config/roles.yaml":
+  file { "${ciab_vagrant_root}/config/roles.yaml":
     source => 'puppet:///modules/bootstrap/classroom_in_a_box/roles.yaml',
   }
 
-  file { "$ciab_vagrant_root/config/pe_build.yaml":
+  file { "${ciab_vagrant_root}/config/pe_build.yaml":
     content => epp('bootstrap/classroom_in_a_box/pe_build.yaml.epp'),
   }
 
-  file { "$ciab_vagrant_root/config/vms.yaml":
+  file { "${ciab_vagrant_root}/config/vms.yaml":
     content => epp('bootstrap/classroom_in_a_box/vms.yaml.epp',
                    { num_students     => $::num_students,
                      num_win_students => $::num_win_students }),
   }
 
+  file { "${ciab_vagrant_root}/bin/start_vagrant_box.sh":
+    mode    => '0755',
+    content => epp('bootstrap/classroom_in_a_box/start_vagrant_box.sh.epp',
+                   { training_home_path => $training_home_path }),
+  }
+
+  # All of these resources need to be enforced before we start
+  # bringing up vagrant boxes
   $vagrant_deps = [
-    File["$ciab_vagrant_root/Vagrantfile"],
-    File["$ciab_vagrant_root/config/pe_build.yaml"],
-    File["$ciab_vagrant_root/config/roles.yaml"],
-    File["$ciab_vagrant_root/config/vms.yaml"],
+    File["${ciab_vagrant_root}/Vagrantfile"],
+    File["${ciab_vagrant_root}/config/pe_build.yaml"],
+    File["${ciab_vagrant_root}/config/roles.yaml"],
+    File["${ciab_vagrant_root}/config/vms.yaml"],
+    File["${ciab_vagrant_root}/bin/start_vagrant_box.sh"],
     Vagrant::Box['current-puppet-master-ova'],
     Vagrant::Box['current-puppet-master-ova']
   ]
@@ -83,9 +93,9 @@ class bootstrap::profile::vagrant {
   # Start up the instructor's Vagrant box
   exec { 'start the master vagrant box':
     user        => 'training',
-    path        => '/bin:/usr/bin',
+    path        => "/bin:/usr/bin:${ciab_vagrant_root}/bin",
     environment => [ "HOME=${training_home_path}" ],
-    command     => "cd ${training_home_path}/classroom_in_a_box && vagrant up master.puppetlabs.vm",
+    command     => "start_vagrant_box.sh master.puppetlabs.vm",
     unless      => "cd ${training_home_path}/classroom_in_a_box && vagrant status master.puppetlabs.vm | grep ^master.puppetlabs.vm 2>/dev/null | awk '{ print $2 }' | grep -q ^running",
     require     => $vagrant_deps,
     logoutput   => true,
@@ -110,9 +120,9 @@ class bootstrap::profile::vagrant {
   range(1, $::num_students - $::num_win_students).each |$n| {
     exec { "start the linux${n}.puppetlabs.vm vagrant box":
       user        => 'training',
-      path        => '/bin:/usr/bin',
+      path        => "/bin:/usr/bin:${ciab_vagrant_root}/bin",
       environment => [ "HOME=${training_home_path}" ],
-      command     => "cd ${training_home_path}/classroom_in_a_box && vagrant up linux${n}.puppetlabs.vm",
+      command     => "start_vagrant_box.sh linux${n}.puppetlabs.vm",
       unless      => "cd ${training_home_path}/classroom_in_a_box && vagrant status linux${n}.puppetlabs.vm | grep ^linux${n}.puppetlabs.vm 2>/dev/null | awk '{ print $2 }' | grep -q ^running",
       require     => $vagrant_deps,
       logoutput   => true,
@@ -122,9 +132,9 @@ class bootstrap::profile::vagrant {
   range($::num_students - $::num_win_students + 1, $::num_students).each |$n| {
     exec { "start the windows${n}.puppetlabs.vm vagrant box":
       user        => 'training',
-      path        => '/bin:/usr/bin',
+      path        => "/bin:/usr/bin:${ciab_vagrant_box}/bin",
       environment => [ "HOME=${training_home_path}" ],
-      command     => "cd ${training_home_path}/classroom_in_a_box && vagrant up windows${n}.puppetlabs.vm",
+      command     => "start_vagrant_box.sh windows${n}.puppetlabs.vm",
       unless      => "cd ${training_home_path}/classroom_in_a_box && vagrant status windows${n}.puppetlabs.vm | grep ^windows${n}.puppetlabs.vm 2>/dev/null | awk '{ print $2 }' | grep -q ^running",
       require     => $vagrant_deps,
       logoutput   => true,
