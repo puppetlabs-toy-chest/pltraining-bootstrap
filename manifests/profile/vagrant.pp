11e16e7d2f08b33b88f1d5bbe7c0a26f4fcb9ae3
Greg Larkin <greg.larkin@puppet.com>
2017-02-23 10:00:41 -0800

    Run the command in the proper directory

--- manifests/profile/vagrant.pp
+++ manifests/profile/vagrant.pp
@@ -160,6 +160,7 @@ class bootstrap::profile::vagrant {
 
   exec { 'generate guacamole ports custom fact':
     user        => 'root',
+    cwd         => $ciab_vagrant_root,
     path        => "/bin:/usr/bin:${ciab_vagrant_root}/bin",
     environment => [ "HOME=${training_home_path}" ],
     command     => 'create_guacamole_ports_fact.sh',
