cdb2846d57f94125570c53ddd9719a22a6b54b02
Greg Larkin <greg.larkin@puppet.com>
2017-02-23 15:50:36 -0800

    Added code to automatically configure the guacamole database with the vagrant box connections and respective user accounts

--- manifests/profile/guacamole.pp
+++ manifests/profile/guacamole.pp
@@ -1,6 +1,7 @@
 class bootstrap::profile::guacamole {
 
   include docker
+
   docker::run {'ciab-guacd':
     image => 'glyptodon/guacd',
     extra_parameters => [
@@ -9,51 +10,55 @@ class bootstrap::profile::guacamole {
   }
 
   if defined('$::guacamole_ports') {
-  docker::run {'ciab-guacamole':
-    image            => 'glyptodon/guacamole',
-    links            => ['ciab-guacd:guacd'],
-    ports            => ['8080:8080'],
-    env              => [
-      "MYSQL_HOSTNAME=${::fqdn}",
-      'MYSQL_DATABASE=guacamole_db',
-      'MYSQL_USER=guacamole_user',
-      'MYSQL_PASSWORD=some_password',
-    ],
-    extra_parameters => [
-      "--add-host ${::fqdn}:${::networking['ip']}",
-    ],
-    require => [Mysql::Db['guacamole_db'],Docker::Run['ciab-guacd']],
-  }
-
-  file { '/usr/src/guacamole':
-    ensure => directory,
-  }
-  file { '/usr/src/guacamole/initdb.sql':
-    ensure => file,
-    source  => 'puppet:///modules/bootstrap/initdb.sql',
-  }
-
-  $override_options = {
-    'mysqld' => {
-      'bind-address' => '0.0.0.0',
+    docker::run {'ciab-guacamole':
+      image            => 'glyptodon/guacamole',
+      links            => ['ciab-guacd:guacd'],
+      ports            => ['8080:8080'],
+      env              => [
+        "MYSQL_HOSTNAME=${::fqdn}",
+        'MYSQL_DATABASE=guacamole_db',
+        'MYSQL_USER=guacamole_user',
+        'MYSQL_PASSWORD=some_password',
+      ],
+      extra_parameters => [
+        "--add-host ${::fqdn}:${::networking['ip']}",
+      ],
+      require => [Mysql::Db['guacamole_db'],Docker::Run['ciab-guacd']],
     }
-  }
-  class {'::mysql::server':
-    override_options => $override_options,
-  }
-  mysql::db {'guacamole_db':
-    user     => 'guacamole_user',
-    password => 'some_password',
-    host     => '%',
-    grant    => ['SELECT','INSERT','UPDATE','DELETE'],
-    sql      => '/usr/src/guacamole/initdb.sql',
-    require  => File['/usr/src/guacamole/initdb.sql'],
-  }
-  firewall { '010 allow mysql':
-    proto  => 'tcp',
-    action => 'accept',
-    dport   => '3306',
-  }
+
+    file { '/usr/src/guacamole':
+      ensure => directory,
+    }
+
+    file { '/usr/src/guacamole/initdb.sql':
+      ensure  => file,
+      content => epp('bootstrap/guacamole_db.sql.epp'),
+    }
+
+    $override_options = {
+      'mysqld' => {
+        'bind-address' => '0.0.0.0',
+      }
+    }
+
+    class {'::mysql::server':
+      override_options => $override_options,
+    }
+
+    mysql::db {'guacamole_db':
+      user     => 'guacamole_user',
+      password => 'some_password',
+      host     => '%',
+      grant    => ['SELECT','INSERT','UPDATE','DELETE'],
+      sql      => '/usr/src/guacamole/initdb.sql',
+      require  => File['/usr/src/guacamole/initdb.sql'],
+    }
+
+    firewall { '010 allow mysql':
+      proto  => 'tcp',
+      action => 'accept',
+      dport   => '3306',
+    }
   }
     
 }
