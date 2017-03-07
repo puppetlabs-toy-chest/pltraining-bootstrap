ace887f637ef2c9c6ba89a9d253f8dd7488a67ca
kjhenner <kjhenner@gmail.com>
2017-03-07 13:57:21 -0800

    fill out puppet_forge_server specs

--- spec/classes/puppet_forge_server_spec.rb
+++ spec/classes/puppet_forge_server_spec.rb
@@ -25,6 +25,45 @@ describe "bootstrap::profile::puppet_forge_server" do
   }
 
   it {
+    is_expected.to contain_user('puppet-forge-server')
+      .with({
+        'ensure'     => 'present',
+        'gid'        => 'puppet-forge-server',
+        'managehome' => 'true',
+      })
+  }
+
+  it {
+    is_expected.to contain_group('puppet-forge-server')
+      .with({
+        'ensure'     => 'present',
+      })
+  }
+
+  it {
+    is_expected.to contain_vcsrepo('/usr/src/puppet-forge-server')
+      .with({
+        'ensure'     => 'present',
+        'provider'   => 'git',
+        'owner'      => 'puppet-forge-server',
+        'group'      => 'puppet-forge-server',
+        'source'     => 'https://github.com/unibet/puppet-forge-server.git',
+        'revision'   => '1.9.0',
+      }).that_requires('user[puppet-forge-server]')
+  }
+
+  it {
+    is_expected.to contain_exec('/usr/local/bin/bundle install')
+      .with({
+        'cwd'         => '/usr/src/puppet-forge-server',
+        'unless'      => '/usr/local/bin/bundle check',
+        'user'        => 'puppet-forge-server',
+        'environment' => "[\"HOME=/home/puppet-forge-server\"]",
+        'logoutput'   => 'on_failure',
+      })
+  }
+
+  it {
     is_expected.to contain_file("/var/opt/forge/")
       .with({
         'ensure' => 'directory',
