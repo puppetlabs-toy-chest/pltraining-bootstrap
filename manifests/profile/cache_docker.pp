7889f8d1f475d2ff57655ddbddeb55231cb95c58
Ben Ford <ben.ford@puppetlabs.com>
2017-03-13 14:27:47 -0700

    Skip docker if unavailable
    TRAINTECH-941 #time 30m

--- manifests/profile/cache_docker.pp
+++ manifests/profile/cache_docker.pp
@@ -1,9 +1,10 @@
 class bootstrap::profile::cache_docker {
 
-  include docker
+  class { 'docker':
+    repo_opt => '--setopt=docker.skip_if_unavailable=true'
+  }
   docker::image { 'maci0/systemd':} # TODO: remove when releasing v5.13
   docker::image { 'centos:7':}
   docker::image { 'phusion/baseimage':}
-
 }
 
