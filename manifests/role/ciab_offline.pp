bcba7fcc071560ac59d16b9c4f4b24799ca43f6d
Greg Larkin <greg.larkin@puppet.com>
2017-03-24 09:59:25 -0700

    Turn off the Guacamole Docker containers before switching to hotspot mode

--- manifests/role/ciab_offline.pp
+++ manifests/role/ciab_offline.pp
@@ -1,5 +1,12 @@
 class bootstrap::role::ciab_offline {
   if str2bool($::offline) {
+    # Turn off the Guacamole Docker containers so they can be
+    # reprovisioned when the CIAB IP address has been changed
+    # after going into hotspot mode
+    service { [ 'docker-ciab-guacamole', 'docker-ciab-guacd' ]:
+      ensure => stopped,
+    }
+
     include bootstrap::profile::create_ap
   }
 }
