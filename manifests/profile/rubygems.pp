ac0ee35572628670e13e3d1f3b1ed6f1d160ab67
Josh Samuelson <js@puppet.com>
2017-03-31 09:21:51 -0700

    cmake3 and cmake are required by showoff (#237)
    * cmake3 and cmake are required by showoff
    
    * Require epel before installing packages
    
    * Depend on epel class instead of yumrepo
    
    * Make sure we have epel

--- manifests/profile/rubygems.pp
+++ manifests/profile/rubygems.pp
@@ -1,9 +1,11 @@
 class bootstrap::profile::rubygems {
   assert_private('This class should not be called directly')
 
+  include epel
   # These are required by rubygems compiling native code
   package { ['cmake3', 'gcc', 'zlib', 'zlib-devel']:
     ensure => present,
+    require => Class['epel'],
   }
 
   # these are used for various scripts
