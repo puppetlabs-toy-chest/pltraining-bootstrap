b166f952373b7ec18b292aa0228e446e6a328805
Greg Larkin <greg.larkin@puppet.com>
2017-04-26 15:41:31 -0700

    Protect against a compiler failure before the ap0 interface exists

--- manifests/profile/ciab_web_interface.pp
+++ manifests/profile/ciab_web_interface.pp
@@ -8,29 +8,31 @@ class bootstrap::profile::ciab_web_interface {
 
   # The CIAB will always have a known address (10.0.0.1) based on the
   # access point
-  $ciab_ip = $::networking['interfaces']['ap0']['bindings'][0]['address']
+  $ciab_ip = $::networking.dig('interfaces', 'ap0', 'bindings', 0, 'address')
 
-  # Contain here because ordering dependencies are set on the
-  # ciab_web_interface class so these resources are applied after
-  # the bootstrap::profile::vagrant class
-  contain nginx
+  if $ciab_ip {
+    # Contain here because ordering dependencies are set on the
+    # ciab_web_interface class so these resources are applied after
+    # the bootstrap::profile::vagrant class
+    contain nginx
 
-  $docroot = '/usr/share/nginx/html'
+    $docroot = '/usr/share/nginx/html'
 
-  file { "${docroot}/index.html":
-    content => epp('bootstrap/ciab_web_interface/index.html.epp',
-                    { ciab_ip => $ciab_ip } ),
-  }
+    file { "${docroot}/index.html":
+      content => epp('bootstrap/ciab_web_interface/index.html.epp',
+		      { ciab_ip => $ciab_ip } ),
+    }
 
-  file { "${docroot}/Puppet-Logo-Amber-White-sm.png":
-    source => 'puppet:///modules/bootstrap/ciab_web_interface/Puppet-Logo-Amber-White-sm.png',
-  }
+    file { "${docroot}/Puppet-Logo-Amber-White-sm.png":
+      source => 'puppet:///modules/bootstrap/ciab_web_interface/Puppet-Logo-Amber-White-sm.png',
+    }
 
-  file { "${docroot}/css":
-    ensure => directory,
-  }
+    file { "${docroot}/css":
+      ensure => directory,
+    }
 
-  file { "${docroot}/css/ciab.css":
-    source => 'puppet:///modules/bootstrap/ciab_web_interface/css/ciab.css',
+    file { "${docroot}/css/ciab.css":
+      source => 'puppet:///modules/bootstrap/ciab_web_interface/css/ciab.css',
+    }
   }
 }
