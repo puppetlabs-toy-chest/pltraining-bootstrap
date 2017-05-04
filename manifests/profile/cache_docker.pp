b0605d060b126914369229386994a390684ab2ba
Josh Samuelson <js@puppet.com>
2017-05-04 14:37:20 -0700

    Remove unused docker images

--- manifests/profile/cache_docker.pp
+++ manifests/profile/cache_docker.pp
@@ -3,9 +3,7 @@ class bootstrap::profile::cache_docker {
   class { 'docker':
     repo_opt => '--setopt=docker.skip_if_unavailable=true'
   }
-  docker::image { 'maci0/systemd':} # TODO: remove when releasing v5.13
   docker::image { 'centos:7':}
-  docker::image { 'phusion/baseimage':}
 
   # Build the docker containers so they are cached
   include puppetfactory::dockerimages
