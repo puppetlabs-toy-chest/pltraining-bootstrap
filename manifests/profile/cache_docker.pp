ec6200d221eed4dc86f5f8406ac4b0ca02bc7b3e
Josh Samuelson <js@puppet.com>
2017-05-04 14:37:37 -0700

    Install gems at build time

--- manifests/profile/cache_docker.pp
+++ manifests/profile/cache_docker.pp
@@ -3,9 +3,8 @@ class bootstrap::profile::cache_docker {
   class { 'docker':
     repo_opt => '--setopt=docker.skip_if_unavailable=true'
   }
-  docker::image { 'centos:7':}
-
-  # Build the docker containers so they are cached
-  include puppetfactory::dockerimages
+  
+  # Build the centos docker container so it is cached
+  include puppetfactory::centosimage
 }
 
