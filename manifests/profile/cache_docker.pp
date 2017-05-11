9eaf74198446b3d2eea5cee124057ec1bc7ec88b
Greg Larkin <greg.larkin@puppet.com>
2017-05-11 16:05:33 -0700

    Added some ordering to make sure the rubygem cache directory is in place before creating the Docker image. The cache directory on the master will be added to the image.

--- manifests/profile/cache_docker.pp
+++ manifests/profile/cache_docker.pp
@@ -3,10 +3,12 @@ class bootstrap::profile::cache_docker {
   class { 'docker':
     repo_opt => '--setopt=docker.skip_if_unavailable=true'
   }
+  contain docker
   
   # Build the centos docker container so it is cached
   class { 'puppetfactory::centosimage':
     master_address => 'master.puppetlabs.vm'
   }
+  contain puppetfactory::centosimage
 }
 
