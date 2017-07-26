ac8691c299aef541a957250728e9c3d9e73875c2
Josh Samuelson <js@puppet.com>
2017-07-26 15:57:29 -0700

    Remove obsolete parameter
    The dockeragent module now builds the "agent" image using "no_agent" as a base image, so this parameter was removed.

--- manifests/profile/dockeragent.pp
+++ manifests/profile/dockeragent.pp
@@ -3,7 +3,6 @@ class bootstrap::profile::dockeragent {
   # We'll probably want to modify this at some point to use published images.
 
   class { 'dockeragent':
-    create_no_agent_image => true,
     lvm_bashrc            => true,
     install_dev_tools     => true,
   }
