b4ae071b67d4c8d6d81a2539b5609a074b47b3ca
Ben Ford <ben.ford@puppetlabs.com>
2017-08-10 12:38:56 -0700

    Specify the proper $pe_build when deploying pe_repo installers

--- manifests/role/master.pp
+++ manifests/role/master.pp
@@ -6,10 +6,7 @@ class bootstrap::role::master {
   include bootstrap::profile::network
   include bootstrap::profile::cache_modules
   include userprefs::defaults
-  include pe_repo::platform::el_6_i386
-  include pe_repo::platform::el_7_x86_64
-  include pe_repo::platform::ubuntu_1404_amd64
-  include pe_repo::platform::windows_x86_64
+  include bootstrap::profile::pe_repo
   include bootstrap::profile::splash
   include bootstrap::profile::pe_tweaks
   include bootstrap::profile::disable_selinux
