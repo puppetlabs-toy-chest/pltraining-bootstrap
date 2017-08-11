7f9cfae8532cbf70d6ad889dbc507bf2311ba0e3
Ben Ford <ben.ford@puppetlabs.com>
2017-08-10 17:49:46 -0700

    add barebones spec testing

---
+++ files/serverspec/master/bootstrap_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe package('pe-puppetserver') do
+  it { should be_installed }
+end
+
+describe package('pe-console-services') do
+  it { should be_installed }
+end
+
+describe package('puppet-agent') do
+  it { should be_installed }
+end
+
+describe port(443) do
+  it { should be_listening }
+end
+
+describe package('wkhtmltopdf') do
+  it { should be_installed }
+end
+
+describe command('wkhtmltopdf --version') do
+  its(:stdout) { should match /with patched qt/ }
+end
+
+describe command('git --git-dir=/var/cache/showoff/courseware/.git --work-tree=/var/cache/showoff/courseware status') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('docker images') do
+  its(:stdout) { should match /centosagent/ }
+end
