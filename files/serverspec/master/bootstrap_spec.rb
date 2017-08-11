5fb8fd3fe7dfcf2dc87d9c51914a4d26d45ed45f
Ben Ford <ben.ford@puppetlabs.com>
2017-08-11 09:55:36 -0700

    Improved spec tests

--- files/serverspec/master/bootstrap_spec.rb
+++ files/serverspec/master/bootstrap_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
+certname  = `puppet agent --configprint certname`.strip
+curlauth  = "--cert /etc/puppetlabs/puppet/ssl/certs/#{certname}.pem "
+curlauth += "--key /etc/puppetlabs/puppet/ssl/private_keys/#{certname}.pem "
+curlauth += "--cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem"
+
 describe package('pe-puppetserver') do
   it { should be_installed }
 end
@@ -24,14 +29,46 @@ describe command('wkhtmltopdf --version') do
   its(:stdout) { should match /with patched qt/ }
 end
 
-describe command('git --git-dir=/var/cache/showoff/courseware/.git --work-tree=/var/cache/showoff/courseware status') do
+describe command('pdftk --version') do
   its(:exit_status) { should eq 0 }
 end
 
-describe command('docker images') do
-  its(:stdout) { should match /centosagent/ }
+describe selinux do
+  it { should be_disabled }
+end
+
+describe service('firewalld') do
+  it { should_not be_enabled }
+end
+
+describe command('puppet module list --modulepath /etc/puppetlabs/code/modules') do
+  its(:stdout) { should_not match /no modules installed/ }
+  its(:exit_status) { should eq 0 }
+end
+
+# validate that the yum mirror is complete and consistent
+describe command('repoclosure -r local') do
+  its(:exit_status) { should eq 0 }
+end
+
+# validate that the courseware git repository has been successfully cloned
+describe command('git --git-dir=/var/cache/showoff/courseware/.git --work-tree=/var/cache/showoff/courseware status') do
+  its(:exit_status) { should eq 0 }
+end
+describe command('git --git-dir=/var/cache/showoff/courseware/.git --work-tree=/var/cache/showoff/courseware remote -v') do
+  its(:stdout) { should match /puppet-training\/courseware.git/ }
 end
 
 describe docker_image('centosagent') do
   it { should exist }
+end
+
+# validate that puppetdb is running properly
+describe command('curl -m 1 http://localhost:8080/pdb/meta/v1/version') do
+  its(:stdout) { should match /version/ }
+end
+
+# check the status api
+describe command("curl https://#{certname}:4433/status/v1/simple #{curlauth}") do
+  its(:stdout) { should match /running/ }
 end
