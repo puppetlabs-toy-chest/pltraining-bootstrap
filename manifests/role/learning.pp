e33e523992f06a079ac666eae527d096e88e730d
Greg Larkin <greg.larkin@puppet.com>
2018-12-05 15:23:36 -0800

    Add gitea to LVM - how did it get installed before??

--- manifests/role/learning.pp
+++ manifests/role/learning.pp
@@ -1,6 +1,7 @@
 class bootstrap::role::learning {
   include userprefs::profile
   include bootstrap
+  include bootstrap::profile::cache_gitea
   include bootstrap::profile::rubygems
   include bootstrap::profile::network
   include bootstrap::profile::pe_tweaks
