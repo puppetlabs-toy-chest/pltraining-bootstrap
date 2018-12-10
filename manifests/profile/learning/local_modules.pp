73af2ec1fc01c636093e622b1c70b351602c2c3a
Greg Larkin <greg.larkin@puppet.com>
2018-12-10 13:59:42 -0800

    Updated to modern versions of the Puppet modules needed for the Learning VM

--- manifests/profile/learning/local_modules.pp
+++ manifests/profile/learning/local_modules.pp
@@ -5,10 +5,11 @@ class bootstrap::profile::learning::local_modules (
   include wget
   
   $learning_modules = [
-    "https://forge.puppet.com/v3/files/puppetlabs-stdlib-4.20.0.tar.gz",
-    "https://forge.puppet.com/v3/files/puppetlabs-concat-2.2.1.tar.gz",
-    "https://forge.puppet.com/v3/files/puppetlabs-postgresql-4.9.0.tar.gz",
-    "https://forge.puppet.com/v3/files/puppetlabs-apt-2.4.0.tar.gz",
+    "https://forge.puppet.com/v3/files/puppetlabs-postgresql-5.11.0.tar.gz",
+    "https://forge.puppet.com/v3/files/puppetlabs-apt-6.2.1.tar.gz",
+    "https://forge.puppet.com/v3/files/puppetlabs-translate-1.2.0.tar.gz",
+    "https://forge.puppet.com/v3/files/puppetlabs-concat-5.1.0.tar.gz",
+    "https://forge.puppet.com/v3/files/puppetlabs-stdlib-5.1.0.tar.gz",
   ]
 
   $learning_modules.each | $module | {
